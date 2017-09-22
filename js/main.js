fbae026d3ba2a1d642827fb066dbdb5ee5f28f20
nitihetemi <nitihetemi@gmail.com>
2017-09-22 17:03:41 +0200

    sm fix

--- js/main.js
+++ js/main.js
@@ -33,21 +33,23 @@ $(".collapse").each(function(){
    }
 });
 
-var sticker = $('#res-sect');
-// var stickerPos = sticker.position();
-// $(window).scroll(function(){
-//    var windowPos = $(window).scrollTop();
-//    if(windowPos >= stickerPos.top) {
-//       sticker.css({'position': 'sticky', 'top': '0px', 'z-index': '99'});
-//    } else {
-//       sticker.css({'height': '80px', 'position': 'relative', 'width': 'auto'});
-//    }
-//    if(windowPos >= stickerPos.top-180) {
-//       $('.scrollup').fadeIn();
-//    } else {
-//       $('.scrollup').fadeOut();
-//    }
-// });
+if(window.location.pathname == '/cee-asset-declarations/') {
+   var sticker = $('#res-sect');
+   var stickerPos = sticker.position();
+   $(window).scroll(function(){
+      var windowPos = $(window).scrollTop();
+      if(windowPos >= stickerPos.top) {
+         sticker.css({'position': 'sticky', 'top': '0px', 'z-index': '99'});
+      } else {
+         sticker.css({'height': '80px', 'position': 'relative', 'width': 'auto'});
+      }
+      if(windowPos >= stickerPos.top-180) {
+         $('.scrollup').fadeIn();
+      } else {
+         $('.scrollup').fadeOut();
+      }
+   });
+}
 
 $('.scrollup').on('click', function(e){
    $("html, body").animate({ scrollTop: 0 }, "slow");
